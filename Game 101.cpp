d6d229da6ab4b8ad827d207dd7f8dbc5ea9e8e56
Dinanka Koirala <dinankakoirala.5@gmail.com>
2026-05-28 17:40:07 +0545

    Initial commit

---
+++ Game 101.cpp
@@ -0,0 +1,12 @@
+﻿// Game 101.cpp : Defines the entry point for the application.
+//
+
+#include "Game 101.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
